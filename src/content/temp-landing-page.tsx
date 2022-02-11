7528b479f9244980042a244a89b18440f0d5e371
Jacob Dunbar <jacobfdunbar@gmail.com>
2022-02-11 09:25:49 -0500

    Updates to temporary landing page.

--- src/content/temp-landing-page.tsx
+++ src/content/temp-landing-page.tsx
@@ -4,9 +4,10 @@ export class TempLandingPage extends React.Component {
     render() {
         return (
             <div id="temp-landing-page">
-                <div id="construction-text">
-                    <h1>Grimbar Interactive</h1>
+                <div>
+                    <img src="Grimbar_Interactive_Logo_Secondary_512.png" alt="Grimbar Interactive Logo"/>
                     <h3>Under Construction...</h3>
+                    <h5><a id="contact" href="mailto:jacob@grimbar.dev">Contact</a></h5>
                 </div>
             </div>
         );
