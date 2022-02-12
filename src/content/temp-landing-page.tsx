58391740e3a73da69c43af30ed687da5cee67e1a
Jacob Dunbar <jacobfdunbar@gmail.com>
2022-02-12 09:20:48 -0500

    More flavorful 'under construction' text.

--- src/content/temp-landing-page.tsx
+++ src/content/temp-landing-page.tsx
@@ -6,7 +6,7 @@ export class TempLandingPage extends React.Component {
             <div id="temp-landing-page">
                 <div>
                     <img src="Grimbar_Interactive_Logo_Secondary_512.png" alt="Grimbar Interactive Logo"/>
-                    <h3>Under Construction...</h3>
+                    <h3>A new journey begins...</h3>
                     <h5><a id="contact" href="mailto:jacob@grimbar.dev">Contact</a></h5>
                 </div>
             </div>
