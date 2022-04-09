58feb0737b0104d5bf76d76d21a13d08230b05d0
Jacob Dunbar <jacobfdunbar@gmail.com>
2022-04-09 18:24:09 -0400

    Updating Village Doctor website image to jpg from png.

--- src/config/websites.ts
+++ src/config/websites.ts
@@ -5,5 +5,5 @@ export const clientWebsites: WebsiteType[] = [{
     client: 'The Village Doctor at Cherry Hill',
     description: 'Reboot of existing website for Family Practice Doctor. This included reworking the information from the original website to give it a more streamlined and modern design, as well and making the site responsive to a variety of screen sizes including mobile. The development work for this was done using React (TypeScript), HTML5, and CSS3.',
     mainURL: 'https://www.thevillagedoctor.net',
-    photoURL: '/images/websites/thevillagedoctor.png'
+    photoURL: '/images/websites/thevillagedoctor.jpg'
 }];
