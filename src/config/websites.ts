f3c7b84a83f8291e70f2bd5b5cf9b413369afdbc
Jacob Dunbar <jacobfdunbar@gmail.com>
2022-04-09 22:05:02 -0400

    Removing https url in favor http for now.

--- src/config/websites.ts
+++ src/config/websites.ts
@@ -4,6 +4,6 @@ export const clientWebsites: WebsiteType[] = [{
     title: 'The Village Doctor Website Rework',
     client: 'The Village Doctor at Cherry Hill',
     description: 'Reboot of existing website for Family Practice Doctor. This included reworking the information from the original website to give it a more streamlined and modern design, as well and making the site responsive to a variety of screen sizes including mobile. The development work for this was done using React (TypeScript), HTML5, and CSS3.',
-    mainURL: 'https://www.thevillagedoctor.net',
+    mainURL: 'http://www.thevillagedoctor.net',
     photoURL: '/images/websites/thevillagedoctor.jpg'
 }];
