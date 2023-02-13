f34eb4d580077b6fb91fd4944efa7b111f7fa4c6
Jacob Dunbar <jacobfdunbar@gmail.com>
2023-02-13 14:47:18 -0500

    Updating LinkedIn link.

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -8,7 +8,7 @@ export default class Footer extends React.Component {
             <div className="footer">
                 <p>Grimbar Interactive LLC est. 2022</p>
                 <div className="image-container">
-                    <a href="https://www.linkedin.com/in/jacob-dunbar-b6b5b2bb/" target="blank"><img src="./images/linkedin-square.png" alt="linkedin"/></a>   
+                    <a href="https://www.linkedin.com/company/grimbar-interactive/" target="blank"><img src="./images/linkedin-square.png" alt="linkedin"/></a>   
                     <a href="https://github.com/Grimbar-Interactive" target="blank"><img src="./images/github-square.png" alt="github"/></a>   
                     <a href="https://twitter.com/GrimbarInt" target="blank"><img src="./images/twitter-square.png" alt="twitter"/></a>
                 </div>    
