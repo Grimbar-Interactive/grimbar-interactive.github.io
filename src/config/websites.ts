af5e0722ade616938b06b3573cdad6c25970fbc7
Amanda Grimm <95390235+amandagrimm95@users.noreply.github.com>
2024-01-22 15:42:25 -0500

    Updating photos and adding CBL site

--- src/config/websites.ts
+++ src/config/websites.ts
@@ -12,4 +12,10 @@ export const clientWebsites: WebsiteType[] = [{
     description: "Amanda's personal portfolio website, built using React(TypeScript) as well as a node.js server.  This site is fully responsive so it will work on any sized device.",
     mainURL: 'https://www.amandadunbar.dev',
     photoURL: '/images/websites/amandadunbar.jpg'
+},{
+    title: 'Committed To Bettering Online Coaching Platform',
+    client: 'CBL Consulting and Training LLC',
+    description: 'This site was built on Kajabi as an all-in-one online coaching platform for Occupational Therapists.  The site handles payments, subscriptions, live coaching sessions, pre-recorded classes, quizzes, certificate distribution, email campaigns and much more.',
+    mainURL: 'https://www.committobettering.com',
+    photoURL: '/images/websites/committedtobettering.jpg'
 }];
