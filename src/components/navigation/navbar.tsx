3f021365d2e9d6a631e29fa8f679670e0f5e467a
Amanda Grimm <95390235+amandagrimm95@users.noreply.github.com>
2024-03-05 08:39:07 -0500

    working on max width

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -64,15 +64,17 @@ export default class NavBar extends React.Component<{}, NavBarState> {
 
 		return (
 			<div id="nav-bar">
-				<Link to="/" id="logo-container" onClick={this.returnUp}>
-					<img id="grimbar-logo" src="./images/Grimbar_Interactive_Logo_Image_Only.png" alt="Grimbar Interactive Logo" />
-					<img id="grimbar-name" src="./images/Grimbar_Interactive_Logo_Name_Only.png" alt="Grimbar Interactive Logo" />
-				</Link>
-				<nav id="desktopNav">{buttons}</nav>
-				<nav id="mobileNav">{buttons}</nav>
-				<div id="icons">
-					<img id="return-up" onClick={this.returnUp} src="./images/return-up-arrow.png" alt="Up Arrow" />
-					<img className="icon" onClick={this.burgerMenu} src="./images/bars-solid.png" alt="Menu Bar" />
+				<div id="nav-bar-container">
+					<Link to="/" id="logo-container" onClick={this.returnUp}>
+						<img id="grimbar-logo" src="./images/Grimbar_Interactive_Logo_Image_Only.png" alt="Grimbar Interactive Logo" />
+						<img id="grimbar-name" src="./images/Grimbar_Interactive_Logo_Name_Only.png" alt="Grimbar Interactive Logo" />
+					</Link>
+					<nav id="desktopNav">{buttons}</nav>
+					<nav id="mobileNav">{buttons}</nav>
+					<div id="icons">
+						<img id="return-up" onClick={this.returnUp} src="./images/return-up-arrow.png" alt="Up Arrow" />
+						<img className="icon" onClick={this.burgerMenu} src="./images/bars-solid.png" alt="Menu Bar" />
+					</div>
 				</div>
 			</div>
 		);
